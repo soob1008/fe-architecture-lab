03b247798a5b7fe365a2cf90be8e495deeedb6dc
subin kim <1008sb354@gmail.com>
2025-09-08 14:22:29 +0900

    fix: import auto

--- src/app/todo/page.tsx
+++ src/app/todo/page.tsx
@@ -1,11 +1,12 @@
 "use client";
 
 import { useEffect, useState } from "react";
+
+import type { TodoVM } from "@/entities/todo/vm/TodoVM";
 import TodoList from "@/features/todo/components/TodoList";
 import { fetchTodos } from "@/features/todo/services/todoService";
-import { isHttpError } from "@/shared/error/AppError";
-import type { TodoVM } from "@/entities/todo/vm/TodoVM";
 import type { AppError } from "@/shared/error/AppError";
+import { isHttpError } from "@/shared/error/AppError";
 
 export default function Day2TodoPage() {
   const [data, setData] = useState<TodoVM[] | null>(null);
